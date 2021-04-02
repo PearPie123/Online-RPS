1306d6cfdfaf8add7b2d57a2be091247ae9fc537
PearPie <aroberts24@mylcusd.net>
2021-04-01 22:16:59 -0700

    added more html/css to change betwen a menu and a game menu when the play button is pressed

--- client/script.mjs
+++ client/script.mjs
@@ -1,15 +1,26 @@
 const socket = io();
-socket.on("test",msg => console.log(msg));
 const playButton = document.getElementById("play");
-const statusDiv = document.getElementById("status");
+const nameInput = document.getElementById("name");
+const menuContainer = document.getElementById("menuContainer");
+const gameContainer = document.getElementById("gameContainer");
 const choiceBtns = document.getElementsByClassName("moveChoice");
 const playersOnlineStatus = document.getElementById("playersOnline");
+const playerStatus = document.getElementById("playerStatus");
+const oponentStatus = document.getElementById("oponentStatus");
+
+socket.on("test",msg => console.log(msg));
 for(let btn of choiceBtns) {
-  btn.onclick = () => {socket.emit("player choice",btn.id);}
+  btn.onclick = () => {
+    socket.emit("player choice",btn.id);
+    playerStatus.textContent = `Choice: ${btn.textContent}`;
+  }
 }
 playButton.onclick = () => {
   socket.emit("play");
-  statusDiv.textContent = "joining game...";
+  menuContainer.classList.remove("mainContainerVisible");
+  menuContainer.classList.add("mainContainerHidden");
+  gameContainer.classList.remove("mainContainerHidden");
+  gameContainer.classList.add("mainContainerVisible");
 }
 socket.on("joined game",(room) => {
   statusDiv.textContent = `Joined game ${room}, enter choice now`;
